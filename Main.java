be26dfbb4f34560151608d6cca529e4ead1b8b5a
purrplestorm <47472044+purrplestorm@users.noreply.github.com>
2020-12-29 12:39:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        block1.main();
+        block2.main();
+        block3.main();
+        block4.main();
+        block5.main();
+        block6.main();
+
+
+    }
+
+
+}
